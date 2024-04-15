08e3a205286dd36a8ecda85e15c761420f3374a4
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2024-04-15 11:10:21 +0300

    components-created

---
+++ src/components/Hizmetlerimiz/index.js
@@ -0,0 +1,19 @@
+import AdblueDpfEgr from "./Adblue-DPF-EGR-Cozumleri"
+import ArizaTespit from "./Arizatespit-ve-cozumu"
+import BurblesPopcorn from "./Burbles&Popcorn"
+import Chiptuning from "./Chiptuning"
+import DSGKart from "./DSG-Sanziman-karti-guncellemesi"
+import HizLimiti from "./Hiz-Limit-Kaldirma"
+import LambdaSistemi from "./Lambda-Sistem-Cozumleri"
+import Paramlama from "./Paramlama"
+
+export {
+    AdblueDpfEgr,
+    ArizaTespit,
+    BurblesPopcorn,
+    Chiptuning,
+    DSGKart,
+    HizLimiti,
+    LambdaSistemi,
+    Paramlama
+}
--- src/sideRoutes.jsx
+++ src/sideRoutes.jsx
@@ -3,6 +3,8 @@ import Anasayfa from "./components/Anasayfa";
 import Randevu from "./components/Randevu";
 import Iletisim from "./components/İletisim";
 import { Alientech, Dimsport, MagicMotorsports, AutoVei } from './components/EcuProgramlama';
+import { AdblueDpfEgr, ArizaTespit, BurblesPopcorn, Chiptuning, DSGKart, HizLimiti, LambdaSistemi, Paramlama } from './components/Hizmetlerimiz';
+
 
 export default function SideRoute() {
     return (
@@ -14,6 +16,15 @@ export default function SideRoute() {
             <Route path="/Dimsport" element={<Dimsport />} />
             <Route path="/MagicMotorsports" element={<MagicMotorsports />} />
             <Route path="/AutoVei" element={<AutoVei />} />
+            <Route path="/AdblueDpfEgr" element={<AdblueDpfEgr />} />
+            <Route path="/ArizaTespit" element={<ArizaTespit />} />
+            <Route path="/BurblesPopcorn" element={<BurblesPopcorn />} />
+            <Route path="/Chiptuning" element={<Chiptuning />} />
+            <Route path="/DSGKart" element={<DSGKart />} />
+            <Route path="/HizLimiti" element={<HizLimiti />} />
+            <Route path="/LambdaSistemi" element={<LambdaSistemi />} />
+            <Route path="/Paramlama" element={<Paramlama />} />
         </Routes>
     );
 }
+
